3d6688d04f6e6ce1bb075d8e3c409fbc83fa1f7d
Ratzmx <enzoxz.023@gmail.com>
2021-09-15 01:43:53 -0300

    v1

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const fetchData = require("./fetchDiff");
+
+const app = express();
+const PORT = 5005;
+
+app.use(express.json());
+
+app.get("/diff", async (req, res) => {
+  const diff = await fetchData();
+  return res.json(diff);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listen on port ${PORT}`);
+});
